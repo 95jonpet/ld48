e1b2d575ced7ac992bb35f3041bb86d95b02f16c
Peter Jonsson <4572231+95jonpet@users.noreply.github.com>
2021-04-25 12:20:28 +0200

    Animate UI transitions

--- Game.gd
+++ Game.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+onready var ui_animation = $Camera/Interface/AnimationPlayer
+
 var active_item_index: int = 0
 var can_place_item: bool = true
 var level_number: int = 1
@@ -27,6 +29,7 @@ func change_level(level: int):
 	
 	if level_file_exists:
 		level_number = level
+		ui_animation.play_backwards("ui-load")
 		SceneChanger.change_scene(level_file_path, $Level, "Level " + str(level))
 	else:
 		# TODO: Handle game end with all levels completed.
@@ -73,6 +76,10 @@ func _on_grid_placeholder_clicked(placeholder):
 		#trigger_level_change_if_needed()
 
 func _on_level_changed(level: Node = null):
+	ui_animation.stop()
 	active_item_index = 0
 	$Camera/Interface.set_inventory_items(level.items if level else $Level.items)
 	$Camera/Interface.set_inventory_active_item(active_item_index)
+	
+	yield(get_tree().create_timer(3), "timeout")
+	ui_animation.play("ui-load")
